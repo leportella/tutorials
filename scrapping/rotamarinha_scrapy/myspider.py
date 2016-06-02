394f6399adaa687cf6c3cd588eae14e4cceded48
leportella <leportella@gmail.com>
2016-06-01 21:14:33 -0300

    almost working

--- scrapping/rotamarinha_scrapy/myspider.py
+++ scrapping/rotamarinha_scrapy/myspider.py
@@ -3,6 +3,7 @@
 
 import scrapy
 from keywords import ocean_keywords as keywords
+from urlparse import urljoin
 
 # para rodar...
 # scrapy runspider top_asked_so_questions.py -t json -o results.json
@@ -17,45 +18,45 @@ class NewsItem(scrapy.Item):
 
 class BBCSpider(scrapy.Spider):
     name = 'bbcnews'
-    allowed_domains = ['bbc.co.uk']
+    allowed_domains = ['bbc.com']
     start_urls = [
-        "http://www.bbc.co.uk",
+        "http://www.bbc.com",
     ]
 
     def parse(self, response):
-            container = response.css("div.container")
-            urls = container.css("a ::attr(href)").extract()
+            container = response.css("div.content")
+            urls = container.css("a[href*=story]::attr(href)").extract() #*= faz uma busca no css (contem)
 
             #coloca dominio pros links proprios do site
             for url in urls:
+            	#print url
                 if url[0]=='/':
-                    next_url = start_urls[0] + url
+                    next_url = urljoin(self.start_urls[0], url)
                 else:
                     next_url = url
 
                 yield scrapy.Request(next_url, callback=self.parse_story)
 
-    def parse_story(self,response):
+    def parse_story(self, response):
 
-        text = ''.join(response.css('div.story-inner p::text').extract())
+        text = u''.join(response.css('div.story-inner p::text').extract())
 
         #nem sempre as noticias estao nas mesmas divs...
         if not text:
-            text = ''.join(response.css('div.story-body__inner p::text').extract())
+            text = u''.join(response.css('div.story-body__inner p::text').extract())
             print 'ok'
 
+        print text         
         # verificar se existem tags relevantes na notícia
         tags = []
         for keyword in keywords:
-            try: 
-                if keyword in text:
-                    tags.append(keyword)
-            except:
-                if str(keyword) in text:
-                    tags.append(keyword) 
+            if keyword in text:
+                tags.append(keyword)
 
+        print tags
         #só salva notícias com mais de duas tags
-        if len(tags) > 2:            
+        story = None
+        if len(tags) > 2:
             story = NewsItem()
             story['url'] = response.url
             story['headline'] = response.xpath("//title/text()").extract()
