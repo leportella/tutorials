e1c8751d0f2b1ba89a53bf6948b5afb1c2267157
Leticia Portella <leportella@gmail.com>
2017-04-17 18:53:18 -0300

    classes from MIT0600

---
+++ mit0600/ps0.py
@@ -0,0 +1,8 @@
+# Problem Set 0
+# Name: Leticia Portella
+# Time spent: 5 min
+
+birth = raw_input('Enter your date of birth: ')
+name = raw_input('Enter your last name: ')
+
+print(name + ' ' + birth)
